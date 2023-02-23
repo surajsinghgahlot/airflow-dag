1414a95ae8a84a62b66e66e3244844d81fbfb149
surajsinghgahlot <58258028+surajsinghgahlot@users.noreply.github.com>
2023-02-23 18:31:45 +0530

    Update dag.py

--- dag.py
+++ dag.py
@@ -3,9 +3,12 @@ from datetime import timedeltafrom
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
-from airflow.utils.dates import days_agoargs = {
+from airflow.utils.dates import days_ago
+
+args = {
     'owner': 'airflow',
 }
+
 dag = DAG(
     dag_id='edgeToEMRandS3',
     default_args=args,
@@ -15,13 +18,16 @@ dag = DAG(
     tags=['somethingForYouToFindYourDAG']
     #params={"example_key": "example_value"},
 )
+
 run_this_first = DummyOperator(
     task_id='run_this_first',
     dag=dag,
 )
+
 now_run_spark_job = BashOperator(
     task_id='now_run_spark_job',
     bash_command='bash /home/ec2-user/airflow/scripts/testEMRtoS3Conn_pyWrapper.sh ',
     dag=dag,
 )
+
 run_this_first >> now_run_spark_job
