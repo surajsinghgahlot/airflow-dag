9f33c72d391873cccf68211da4c67d3bd362180e
Suraj Singh <suraj@tmdc.io>
2023-02-23 21:01:00 +0530

    testing

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
+from datetime import timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
 
 args = {
     'owner': 'suraj',
@@ -8,6 +10,11 @@ args = {
 
 dag = DAG(
     dag_id='testing',
+    default_args=args,
+    schedule_interval=None,
+    start_date=days_ago(2),
+    dagrun_timeout=timedelta(minutes=60),
+    tags=['for testing purpose']
 )
 
 run_this_first = DummyOperator(
