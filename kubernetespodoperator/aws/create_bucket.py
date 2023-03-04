ca9f93ad7f372b5fdfa6262c1410fa61472470ef
Suraj Singh <suraj@tmdc.io>
2023-03-04 22:45:35 +0530

    removed image screts

--- kubernetespodoperator/aws/create_bucket.py
+++ kubernetespodoperator/aws/create_bucket.py
@@ -30,7 +30,6 @@ create_s3_bucket = KubernetesPodOperator(
     image='ss14suraj/boto3:s3',
     cmds=["python"],
     arguments=["create_bucket.py"],
-    # image_pull_secrets=[k8s.V1LocalObjectReference("imagescretsname")],
     env_vars={
         "AWS_ACCESS_KEY": Variable.get("ACCESS_KEY"),
         "AWS_SECRET_KEY": Variable.get("SECRET_KEY"),
