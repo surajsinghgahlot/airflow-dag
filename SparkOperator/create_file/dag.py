7307ad2848691718b563f4ddad6afcfa6c6e84f9
Suraj Singh <suraj@tmdc.io>
2023-03-11 02:29:49 +0530

    files changed

--- SparkOperator/create_file/dag.py
+++ SparkOperator/create_file/dag.py
@@ -13,7 +13,7 @@ args = {
 }
 
 dag = DAG(
-    dag_id="base_spark_operator",
+    dag_id="create_file",
     default_args=args,
     schedule_interval=None,
     start_date=days_ago(2),
@@ -27,7 +27,7 @@ run_this_first = DummyOperator(
 )
 
 base_spark_job = SparkSubmitOperator(
-	application ='/opt/bitnami/airflow/dags/git_airflow-dag/SparkOperator/base/basic_submit_job.py',
+	application ='/opt/bitnami/airflow/dags/git_airflow-dag/SparkOperator/create_file/spark_job.py',
 	conn_id= 'spark_default',
 	task_id='base_spark_job',
 	dag=dag
---
+++ SparkOperator/read_csv/dag.py
@@ -0,0 +1,36 @@
+from datetime import timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
+from airflow.utils.dates import days_ago
+
+os.environ["PYSPARK_PYTHON"]="/opt/bitnami/airflow/venv/bin/python3.9"
+os.environ["JAVA_HOME"]="/usr"
+
+args = {
+    'owner': 'SparkOperator',
+}
+
+dag = DAG(
+    dag_id="csv_read",
+    default_args=args,
+    schedule_interval=None,
+    start_date=days_ago(2),
+    dagrun_timeout=timedelta(minutes=60),
+    tags=["for testing purpose"]
+)
+
+run_this_first = DummyOperator(
+    task_id='run_this_first',
+    dag=dag
+)
+
+csv_read = SparkSubmitOperator(
+	application ='/opt/bitnami/airflow/dags/git_airflow-dag/SparkOperator/read_csv/spark_job.py',
+	conn_id= 'spark_default',
+	task_id='csv_read',
+	dag=dag
+)
+
+run_this_first >> csv_read
