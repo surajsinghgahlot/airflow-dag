9f143055e9ac392209a8cae6f28b466ae51b5076
Suraj Singh <suraj@tmdc.io>
2023-03-11 00:23:15 +0530

    csv file path

--- SparkOperator/base/basic_submit_job.py
+++ SparkOperator/base/basic_submit_job.py
@@ -1,7 +1,7 @@
 import pyspark
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('abc').getOrCreate()
-df = spark.read.csv("python.csv", header=True)
+df = spark.read.csv("/opt/bitnami/airflow/dags/git_airflow-dag/SparkOperator/base/python.csv", header=True)
 df.printSchema()
 print(df.show())
 spark.stop
